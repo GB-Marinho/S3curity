3c7a348334c41d2784c41af6d602a6690652219e
Pedro Lucas G. Prata <78231145+PedroSCY@users.noreply.github.com>
2024-12-12 17:06:14 -0300

    Correcao Layout
    * Main não invade mais o cabeçalho nem o sidbar.

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -7,7 +7,7 @@ import { Combobox } from "../ui/combobox";
 
 export default function Sidebar() {
   return (
-    <div className="bg-black w-[300px] flex flex-col">
+    <div className="bg-black w-[300px] flex flex-col flex-none h-screen">
       <div className="flex flex-col gap-2 h-[175px] justify-center items-center">
         <Image src={Logo} alt="Logo" width={90} />
         <Image src={Logotipo} alt="Logotipo" width={90} />
