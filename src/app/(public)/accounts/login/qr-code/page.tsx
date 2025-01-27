fe727db8289e8b90a3b87701302b4b4559aa4d3a
Braulio Henrique Marques Souto <braulio@disroot.org>
2025-01-27 17:11:18 -0300

    Implementado Login por Qr Code - faltam ajustes

---
+++ src/app/(public)/accounts/login/qr-code/page.tsx
@@ -0,0 +1,30 @@
+import { LoginQrCodeForm } from "@/components/forms/loginForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { next: string };
+}) {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center p-12 gap-8">
+      <Card className="w-full sm:w-[350px]">
+        <CardHeader>
+          <CardTitle>Autenticação Por QrCode</CardTitle>
+          <CardDescription>
+            Aponte o Qr-Code para realizar o login
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="w-full flex items-center justify-center h-full">
+          <LoginQrCodeForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
