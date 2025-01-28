9629b96c489f3b475a09489af8307291cbc8cb1e
Braulio Henrique Marques Souto <braulio@disroot.org>
2025-01-27 23:13:40 -0300

    Finalizado Login QrCode

--- src/components/forms/loginForm/loginQrCodeForm/LoginQrCodeForm.tsx
+++ src/components/forms/loginForm/loginQrCodeForm/LoginQrCodeForm.tsx
@@ -1,15 +1,26 @@
 "use client";
-import Html5QrcodePlugin from "@/components/QrCodePlugin/Html5QrcodePlugin";
 
-export function LoginQrCodeForm() {
-  const onNewScanResult = (decodedText, decodedResult) => {
-    // handle decoded results here
-    console.log("decodeText", decodedText);
-    console.log("decodeResult", decodedResult);
-  };
+import { Html5QrcodePlugin } from "@/components/qrCode";
+import { useAuth } from "@/context";
+import { handleError } from "@/lib";
+
+interface LoginQrCodeFormProps {
+  next?: string;
+}
+
+export function LoginQrCodeForm(props: LoginQrCodeFormProps) {
+  const { loginQrCode } = useAuth();
+
+  async function onNewScanResult(decodedText: string) {
+    try {
+      await loginQrCode(decodedText, props.next);
+    } catch (error) {
+      handleError(error);
+    }
+  }
 
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full flex justify-center mx-auto mt-4">
       <Html5QrcodePlugin
         fps={10}
         qrbox={250}
--- src/context/AuthContext/AuthContext.tsx
+++ src/context/AuthContext/AuthContext.tsx
@@ -7,6 +7,7 @@ import {
 import { createCookie, deleteCookie, getCookie } from "@/lib/actions/";
 import { decrypt } from "@/lib/JWT/verifyToken";
 import { loginRequest, LoginResponse, otpValidation } from "@/services";
+import { loginByQrCodeRequest } from "@/services/accounts/loginByQrCode";
 import {
   AuthContextInterface,
   AuthProviderInterface,
@@ -105,6 +106,34 @@ export function AuthProvider({ children }: AuthProviderInterface) {
     }
   }
 
+  async function loginQrCode(token: string, next?: string) {
+    const response = await loginByQrCodeRequest(token);
+    if (response.status === 200) {
+      const data = response.data as LoginResponse;
+      await createCookie("tokenId", data.tokenId);
+      await createCookie("token", data.token, 30 * 24 * 60 * 60); // 30 dias
+      const payload = await decrypt(data.tokenId);
+      setToken(data.tokenId);
+      setUser({
+        id: payload.id as string,
+        name: payload.nome as string,
+        email: payload.email as string,
+      });
+      // replace(PATH_PAGE_HOME);
+      window.location.replace(next || PATH_PAGE_HOME);
+    } else if (response.status === 303) {
+      const data = response.data as LoginResponse;
+      const urlParams = new URLSearchParams(searchParams);
+      urlParams.set("email", data.tokenId); //tokenId nesse caso é o email
+      push(
+        `${PATH_PAGE_ACCOUNTS_LOGIN_2FA_VERIFICATION}?${urlParams.toString()}`
+      );
+    } else {
+      const error = response.data as ErrorResponse;
+      toast.error(error.message);
+    }
+  }
+
   async function logout() {
     await deleteCookie("tokenId");
     await deleteCookie("token");
@@ -114,7 +143,15 @@ export function AuthProvider({ children }: AuthProviderInterface) {
 
   return (
     <AuthContext.Provider
-      value={{ ...user, isAuthenticated, token, loading, login, logout }}
+      value={{
+        ...user,
+        isAuthenticated,
+        token,
+        loading,
+        login,
+        loginQrCode,
+        logout,
+      }}
     >
       {children}
     </AuthContext.Provider>
